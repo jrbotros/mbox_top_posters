4c5b7650833eec8a7869d75365293f9adc790704
Joseph Botros <jrbotros@gmail.com>
2014-06-24 23:38:01 -0400

    remove test filewrite

--- top_posters.py
+++ top_posters.py
@@ -12,34 +12,33 @@ def post_counts(start_date, end_date):
 
     senders = {}
     with open('latest.mbox', 'r') as archive:
-        with open('post_counts.txt', 'w') as outfile:
-            cur_sender = None
-            cur_date = None
-            for line in archive.readlines():
-                parsed_sender = SENDER_REGEX.match(line)
-                if parsed_sender:
-                    cur_sender = parsed_sender
-                else:
-                    parsed_date = DATE_REGEX.match(line)
-                    if parsed_date:
-                        try:
-                            cur_date = parse(parsed_date.group('date'))
-                        except ValueError:
-                            print parsed_date.group('date')
-                    elif EMPTY_REGEX.match(line):
-                        if cur_sender and cur_date and start_date < cur_date < end_date:
-                            name = cur_sender.group('name')
-                            email = cur_sender.group('email')
-                            if not name:
-                                name = email
-                            if senders.get(name):
-                                if email not in senders[name]['emails']:
-                                    senders[name]['emails'].append(email)
-                                senders[name]['count'] += 1
-                            else:
-                                senders[name] = {'emails': [email], 'count': 1}
-                            cur_sender = None
-                            cur_date = None
+        cur_sender = None
+        cur_date = None
+        for line in archive.readlines():
+            parsed_sender = SENDER_REGEX.match(line)
+            if parsed_sender:
+                cur_sender = parsed_sender
+            else:
+                parsed_date = DATE_REGEX.match(line)
+                if parsed_date:
+                    try:
+                        cur_date = parse(parsed_date.group('date'))
+                    except ValueError:
+                        print parsed_date.group('date')
+                elif EMPTY_REGEX.match(line):
+                    if cur_sender and cur_date and start_date < cur_date < end_date:
+                        name = cur_sender.group('name')
+                        email = cur_sender.group('email')
+                        if not name:
+                            name = email
+                        if senders.get(name):
+                            if email not in senders[name]['emails']:
+                                senders[name]['emails'].append(email)
+                            senders[name]['count'] += 1
+                        else:
+                            senders[name] = {'emails': [email], 'count': 1}
+                        cur_sender = None
+                        cur_date = None
     return senders
 
 def get_top(start_date=datetime.min, end_date=datetime.now(), n=None):
